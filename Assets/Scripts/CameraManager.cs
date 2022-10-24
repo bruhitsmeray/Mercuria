ac86c27c9de0757f34297aaf38f07c76db755eac
Morar Andrei <whiteex@icloud.com>
2022-10-24 17:22:53 +0300

    October 24th, 2022 (4)
    - Add Camera Manager

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform orientation;
+    
+    private float _xRotation = .0f;
+    private float _yRotation = 0.0f;
+
+    [Header("Sensitivity")]
+    [Tooltip("This box will be taken into account only if the 'useUnifiedSensitivity' is checked.")]
+    public float sensitivity = 160.0f;
+    public float verticalSensitivity = 160.0f; // Y axis.
+    public float horizontalSensitivity = 160.0f; // X axis.
+
+    [Header("Preferences")]
+    [Tooltip("This checkbox will allow you to choose if you want to use the Sensitivity for both Horizontal and Vertical movement.")]
+    public bool useUnifiedSensitivity = true;
+
+    private void Start() {
+        //orientation = GetComponent<Transform>().Find("Orientation");
+        SetCursor(true);
+    }
+
+    private void Update() {
+        Look();
+        UnifySens();
+    }
+
+    public void UnifySens() {
+        if (useUnifiedSensitivity) {
+            verticalSensitivity = sensitivity;
+            horizontalSensitivity = sensitivity;
+        }
+    }
+
+    public void Look() {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.smoothDeltaTime * horizontalSensitivity;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.smoothDeltaTime * verticalSensitivity;
+        
+        _xRotation -= mouseY;
+        _yRotation += mouseX;
+        _xRotation = Mathf.Clamp(_xRotation, -90.0f, 90.0f);
+
+        transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, _yRotation, 0);
+    }
+
+    public void SetCursor(bool isHidden) {
+        if (isHidden) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        } else {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
